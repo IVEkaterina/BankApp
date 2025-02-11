99f14609a35fd01841830a2c3e6c6e5f5b683772
Ivanova Ekaterina <kkarmyjjk@gmail.com>
2025-02-11 15:45:06 +0300

    add new functions

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from masks import get_mask_card_number
+from masks import get_mask_account
+
+
+def mask_account_card(text: str) -> str:
+    new_text = text.split()
+    if new_text[0] == "Счет":
+        last = get_mask_account(new_text[-1])
+    else:
+        last = get_mask_card_number(new_text[-1])
+    del new_text[-1]
+    return " ".join(new_text) + " " + last
+
+
+def get_date(date: str) -> str:
+    new_date = date.split("-")
+    day = (new_date[2])[0:2]
+    result = [day, new_date[1], new_date[0]]
+    return ".".join(result)
