e42095708ca8a3d6447e431c2c5e7d98c0a794bb
Ekaterina <kkarmyjjk@gmail.com>
2025-02-27 11:26:51 +0300

    big changes src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,23 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    card_number = card_number[:6] + "******" + card_number[-4:]
-    result = " ".join(card_number[i: i + 4] for i in range(0, 16, 4))
-
+    if card_number.isdigit():
+        if len(card_number) == 16:
+            card_number = card_number[:6] + "******" + card_number[-4:]
+            result = " ".join(card_number[i: i + 4] for i in range(0, 16, 4))
+        else:
+            result = "Введите 16-значный номер карты"
+    else:
+        result = "Введите номер карты"
     return result
 
 
 def get_mask_account(account: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
-    account = "**" + str(account[-4:])
-
+    if account.isdigit():
+      if len(account) == 20:
+        account = "**" + str(account[-4:])
+      else:
+        account = "Введите 20-значный номер счета"
+    else:
+      account = "Введите номер счета"
     return account
