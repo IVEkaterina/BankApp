a917b41895b30474c1e73d37a8c4445283ab27ac
Ekaterina <kkarmyjjk@gmail.com>
2025-03-05 16:18:22 +0300

    finished writing generators and tests for generators

---
+++ tests/test_generators.py
@@ -0,0 +1,35 @@
+from collections.abc import Generator
+
+import pytest
+
+from generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+def test_filter_by_currency_usd(transactions):
+    result = list(filter_by_currency(transactions, "USD"))
+    assert len(result) == 3
+
+
+def test_filter_by_currency_empty(transactions):
+    result = list(filter_by_currency(transactions, "EUR"))
+    assert len(result) == 0
+
+
+def test_filter_by_currency_empty_list():
+    result = list(filter_by_currency([], "USD"))
+    assert len(result) == 0
+
+
+def test_transaction_descriptions(transactions):
+    generator_transaction_descriptions = transaction_descriptions(transactions)
+    assert next(generator_transaction_descriptions) == "Перевод организации"
+    assert next(generator_transaction_descriptions) == "Перевод со счета на счет"
+    assert next(generator_transaction_descriptions) == "Перевод со счета на счет"
+    assert next(generator_transaction_descriptions) == "Перевод с карты на карту"
+    assert next(generator_transaction_descriptions) == "Перевод организации"
+
+
+def test_card_number_generator():
+    generator_card_number_generator = card_number_generator(1, 5)
+    assert next(generator_card_number_generator) == "0000 0000 0000 0001"
+    assert next(generator_card_number_generator) == "0000 0000 0000 0002"
