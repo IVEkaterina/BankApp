837b3715267ee0e4487cf81d9e4a38d9224614ff
Ekaterina <kkarmyjjk@gmail.com>
2025-03-06 10:44:54 +0300

    rewrote functions

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(text: str) -> str:
+    """Функцию, которая умеет обрабатывать информацию как о картах, так и о счетах."""
     new_text = text.split()
     if new_text[0] == "Счет":
         last = get_mask_account(new_text[-1])
@@ -12,6 +13,7 @@ def mask_account_card(text: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """Функция принимает на вход строку с датой и возвращает строку с датой в нужном формате"""
     new_date = date.split("-")
     day = (new_date[2])[0:2]
     result = [day, new_date[1], new_date[0]]
