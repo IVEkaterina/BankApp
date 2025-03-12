6900fd2f8e65f2f9a4609ccdc12d24d5eb3246f7
Ekaterina <kkarmyjjk@gmail.com>
2025-03-12 16:24:41 +0300

    wrote tests for decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,10 +1,31 @@
 import pytest
-from src.decorators import log, my_function
 
-def test_log(capsys):
-    with pytest.raises(Exception):
-        captured = capsys.readouterr()
-        assert captured.out == f"my_function error: {e}. Inputs:{args}, {kwargs}\n"
-    @log
-    def my_function(x = "6", y = [1, 2, 3]):
-        return x + y
+from src.decorators import addition, division, multiplication, subtraction
+
+
+def test_addition(capsys):
+    addition(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "addition ok\n"
+
+
+def test_division(capsys):
+    with pytest.raises(ZeroDivisionError):
+        division(2, 0)
+    captured = capsys.readouterr()
+    assert "division error: ZeroDivisionError. Inputs: (2, 0), {}\n" in captured.out
+
+
+def test_multiplication():
+    multiplication(2, 3)
+    with open("mylog.txt", "r") as file:
+        lines = file.readlines()
+        assert lines[-1] == "multiplication ok\n"
+
+
+def test_subtraction():
+    with pytest.raises(TypeError):
+        subtraction("a", 1)
+    with open("error.txt", "r") as file:
+        lines = file.readlines()
+        assert "subtraction error: TypeError. Inputs: ('a', 1), {}" in lines[-1]
