b735be48f2e9f637d737e1d471dc2611daaa9d3a
Ekaterina <kkarmyjjk@gmail.com>
2025-04-22 22:32:43 +0300

    rewrote docstring

--- src/finance.py
+++ src/finance.py
@@ -4,7 +4,19 @@ import pandas as pd
 
 
 def read_transactions_from_csv(file_path: str, encoding="utf-8") -> list[dict]:
-    """Читает CSV-файл с финансовыми операциями и возвращает список словарей."""
+    """Считывает CSV-файл с финансовыми операциями и возвращает список словарей.
+
+        Аргументы:
+            file_path (str): Путь к CSV-файлу.
+            encoding (str): Кодировка файла. По умолчанию "utf-8".
+
+        Возвращает:
+            list[dict]: Список словарей, каждый из которых представляет одну финансовую операцию.
+
+        Исключения:
+            FileNotFoundError: Если указанный файл не найден.
+            Exception: Любая другая ошибка при чтении файла.
+        """
     try:
         with open(file_path, encoding=encoding) as file:
             reader = csv.DictReader(file, delimiter=";")
@@ -20,7 +32,19 @@ print(read_transactions_from_csv("../data/transactions.csv"))
 
 
 def read_transactions_from_excel(file_path: str) -> list[dict]:
-    """Считывает финансовые операции из XLSX-файла и возвращает список словарей."""
+    """Считывает XLSX-файл с финансовыми операциями и возвращает список словарей.
+
+        Аргументы:
+            file_path (str): Путь к Excel-файлу (.xlsx).
+
+        Возвращает:
+            list[dict]: Список словарей, каждый из которых представляет одну финансовую операцию.
+
+        Исключения:
+            UnicodeDecodeError: Ошибка кодировки при чтении файла.
+            FileNotFoundError: Если указанный файл не найден.
+            Exception: Любая другая ошибка при чтении файла.
+        """
     try:
         df = pd.read_excel(file_path)
         return df.to_dict(orient="records")
