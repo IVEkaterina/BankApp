a31b78fc8deec2a09036a7e4f051755715e40605
unknown <Ivanova.E@SH.HUB>
2025-04-22 17:57:13 +0300

    wrote the functions

---
+++ src/finance.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import csv
+
+def read_transactions_from_csv(file_path: str, encoding="utf-8") -> list[dict]:
+    """Читает CSV-файл с финансовыми операциями и возвращает список словарей."""
+    try:
+        with open(file_path, encoding=encoding) as file:
+            reader = csv.DictReader(file, delimiter=";")
+            return list(reader)
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+        return []
+    except Exception as ex:
+        print(f"Произошла ошибка: {ex}")
+        return []
+
+print(read_transactions_from_csv("../data/transactions.csv"))
+
+
+def read_transactions_from_excel(file_path: str) -> list[dict]:
+    """Считывает финансовые операции из XLSX-файла и возвращает список словарей."""
+    try:
+        # Чтение Excel-файла в DataFrame
+        df = pd.read_excel(file_path)
+        # Преобразование DataFrame в список словарей
+        return df.to_dict(orient="records")
+    except UnicodeDecodeError as ex:
+        print(f"Ошибка кодировки: {ex}")
+    except FileNotFoundError:
+        print(f"Файл не найден: {file_path}")
+    except Exception as ex:
+        print(f"Произошла ошибка: {ex}")
+
+print(read_transactions_from_excel("../data/transactions_excel.xlsx"))
